3b62f324f604819f640bde3e440ac37c28499d2d
Jonathan Larson <jonathan.larson@riseinteractive.com>
2016-08-11 16:55:16 -0500

    Additional Changes

--- index.php
+++ index.php
@@ -8,12 +8,21 @@
 </head>
 <body onLoad='initialLoad()'>
   <div id="content">
+	  <div class="header">
+		<h3>HIGH &nbsp;&nbsp;&nbsp;&nbsp; FIVES</h3>
+		<img id="logo" src="15Five-Logo.png" />
+	  </div>
 	 <div class="wrapper">
 		<div class="highfive">
-			<div class="recip">
+			<!-- <div class="recip">
 				<h1 class="recipH"></h1>
+			</div> -->
+			<div class="redeemedCircle">
+				<img class="redeemedIcon" src="box-04.png" />
+				<img class="redeemedIcon2 displayNone" src="check-04.png" />
 			</div>
 			<div class="textWrapper">
+
 				<p id="hfText">
 					Loading...
 				</p>
@@ -26,6 +35,7 @@
 			</div>
 		</div>
 	</div>
+	<!-- <img id="hand" src="hand-03.png" /> -->
   </div>
 </body>
 
