2ee06d7acee6d7e684d49afa1c760bd72a372610
John Horton <John.Horton@riseinteractive.com>
2016-08-12 16:30:15 -0500

    Added the update function to the 15Five API

--- 15Five.php
+++ 15Five.php
@@ -44,43 +44,48 @@ $foundNew = 1;
 
 // Loop through the JSON Results and inspect them
 foreach ($json['results'] as &$result) {
-    $fiveDate = strtotime($fiveDate = $result['create_ts']);
-
-
-    if ($datetime < $fiveDate) {
-        //echo $fiveText;
+    $fiveDate = strtotime($result['create_ts']);
+    $fiveText = $result['text'];
+    $fiveQty = substr_count($fiveText, "@");
+    //check if the hashtag is in the text
+    if (stripos($fiveText, "#highfive") > -1 || true) {
         $fiveTS      = $result['create_ts'];
         $fiveID      = $result['id'];
         $fiveCreator = $result['creator_id'];
-        $fiveText    = $result['text'];
-        //check if the hashtag is in the text
-        if (stripos($fiveText, "#highfive") || true) {
-            //Add the new high fives to the database
-            $filterText = preg_replace('/(^|\s)@(\w*[a-zA-Z_]+\w*)/', '\1<span>@\2</span>', $fiveText);
+        $filterText  = preg_replace('/(^|\s)@(\w*[a-zA-Z_]+\w*)/', '\1<span>@\2</span>', $fiveText);
+        if (strtotime($datetime) < $fiveDate) {
             $query2 = "INSERT INTO highfives (id, creator_id, text, creation_ts, redeemed, qty)
-          VALUES (:id, :user, :text, :ts, :redeemed, :qty)";
-
+            VALUES (:id, :user, :text, :ts, :redeemed, :qty)";
             $params = array(
                 ':id' => $fiveID,
                 ':user' => $fiveCreator,
                 ':text' => $filterText,
                 ':ts' => $fiveTS,
                 ':redeemed' => 0,
-                ':qty' => 1
+                ':qty' => $fiveQty
+            );
+            $stmt   = $db->prepare($query2);
+            $stmt->execute($params);
+
+        } else {
+            //if its older than we've recorded, update it (in case it changed)
+            $query2 = "UPDATE highfives SET text=:update WHERE id = :id";
+            $params = array(
+                ':id' => $fiveID,
+                ':update' => $filterText
             );
             $stmt   = $db->prepare($query2);
             $stmt->execute($params);
         }
-        //Update the database settings lastSearchDate
-
+          //Update the database settings lastSearchDate
         $query3 = "UPDATE settings SET lastSearchDate = :ts";
         $params = array(
             ':ts' => $fiveTS
         );
         $stmt   = $db->prepare($query3);
         $stmt->execute($params);
-
     }
+
 }
 $query = "SELECT highfives.text, highfives.redeemed, highfives.qty, DATE_FORMAT(highfives.creation_ts, '%W, %M %e %l%p') as 'date', users.first_name, users.last_name FROM highfives
             LEFT JOIN users ON highfives.creator_id = users.id
@@ -90,7 +95,7 @@ $stmt->execute();
 if ($stmt) {
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     header('Content-Type: application/json');
-    $json    = json_encode($results);
+    $json = json_encode($results);
     echo $json;
 }
 
